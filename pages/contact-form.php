8f857692efc884427a33bb04ae10bfadd0b425ef
James KANDUKI <51404903+JamesKanduki@users.noreply.github.com>
2021-05-17 16:52:36 +0200

    Add files via upload

---
+++ pages/contact-form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+								<form class="contact_form_inner" action="">
+                                    <fieldset>
+                                       <div class="field">
+                                          <input type="text" name="name" placeholder="Your name" />
+                                       </div>
+                                       <div class="field">
+                                          <input type="email" name="email" placeholder="Email" />
+                                       </div>
+                                       <div class="field">
+                                          <input type="text" name="phone_no" placeholder="Phone number" />
+                                       </div>
+                                       <div class="field">
+                                          <textarea name="message" placeholder="Message"></textarea>
+                                       </div>
+                                       <div class="field center">
+                                          <button class="margin-top_30">SEND</button>
+                                       </div>
+                                    </fieldset>
+                                 </form>
+
+</body>
+</html>
