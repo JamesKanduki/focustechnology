f4c3809971941700a5e93108b1267ba3376375ae
James KANDUKI <51404903+JamesKanduki@users.noreply.github.com>
2021-05-17 16:34:15 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+   <!-- Basic -->
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- Mobile Metas -->
+      <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+      <!-- Site Metas -->
+      <title>Contacts</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- Site Icons -->
+      <link rel="shortcut icon" href="#" type="image/x-icon" />
+      <link rel="apple-touch-icon" href="#" />
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="css/bootstrap.min.css" />
+      <!-- Pogo Slider CSS -->
+      <link rel="stylesheet" href="css/pogo-slider.min.css" />
+      <!-- Site CSS -->
+      <link rel="stylesheet" href="css/style.css" />
+      <!-- Responsive CSS -->
+      <link rel="stylesheet" href="css/responsive.css" />
+      <!-- Custom CSS -->
+      <link rel="stylesheet" href="css/custom.css" />
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+   </head>
+   <body id="inner_page" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
+      <!-- LOADER -->
+      <div id="preloader">
+         <div class="loader">
+            <img src="images/loader.gif" alt="#" />
+         </div>
+      </div>
+      <!-- END LOADER -->
+      <div class="wrapper">
+      <?php 
+         include('pages/side-header.php');
+      ?>
+         <!-- End header -->
+         <!-- Inner page banner -->
+         <div id="inner_page_banner" class="section" style="background-image:url(images/slider_1.png);background-size: cover;background-position: center center;">
+            <div class="container">
+               <div class="row">
+                  <div class="col-sm-12">
+                      <div class="full">
+                        <h3>Contact us</h3>
+                      </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- End inner page banner -->
+         <!-- section -->
+         <div class="section contact_section">
+            <div class="container-fluid">
+               <div class="row">
+                  <div class="col-md-6 padding_0">
+                     <div class="full">
+                        <div class="row">
+                           <div class="col-sm-12">
+                              <div class="full map_section">
+                                 <div id="map">
+                                    <div id="googleMap" style="width:100%;height:440px;"></div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-md-6 padding_0">
+                     <div class="full">
+                        <div class="row">
+                           <div class="col-sm-12 col-md-10 offset-lg-1">
+                              <div class="full contact_form">
+                                 <?php
+                                    include('pages/contact-form.php');
+                                 ?>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- end section -->
+         <!-- Start Footer -->
+         <?php
+            include('pages/footer.php');
+         ?>
+            </div>
+         </footer>
+         <!-- End Footer -->
+         <div class="footer_bottom">
+            <div class="container">
+               <div class="row">
+                  <div class="col-12">
+                     <p class="crp">© Copyrights 2019 design by html.design</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <a href="#" id="scroll-to-top" class="hvr-radial-out"><i class="fa fa-angle-up"></i></a>
+      <!-- ALL JS FILES -->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <!-- ALL PLUGINS -->
+      <script src="js/jquery.magnific-popup.min.js"></script>
+      <script src="js/jquery.pogo-slider.min.js"></script>
+      <script src="js/slider-index.js"></script>
+      <script src="js/smoothscroll.js"></script>
+      <script src="js/form-validator.min.js"></script>
+      <script src="js/contact-form-script.js"></script>
+      <script src="js/isotope.min.js"></script>
+      <script src="js/images-loded.min.js"></script>
+      <script src="js/custom.js"></script>
+      <!-- End Google Map -->
+      <script>
+         $(document).ready(function() {
+           $('#sidebarCollapse').on('click', function() {
+             $('#sidebar, #content').toggleClass('active');
+             $('.collapse.in').toggleClass('in');
+             $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+           });
+         });
+      </script>
+      <!-- google map js -->
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8eaHt9Dh5H57Zh0xVTqxVdBFCvFMqFjQ&callback=initMap"></script>
+      <!-- end google map js -->
+   </body>
+</html>
---
+++ focus-house.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="row">
+                  <div class="col-md-6">
+                     <div class="full text_align_right_img">
+                        <img src="images/IMG_7477F.jpg" width="900px" height="549px" alt="#" />
+                     </div>
+                  </div>
+                  <div class="col-md-6">
+                     <div class="full paddding_left_15">
+                        <div class="heading_main text_align_left">
+                           <h2><span class="theme_color">Welcome to</span><br> OUR house</h2>
+                        </div>
+                     </div>
+                     <div class="full paddding_left_15">
+                        <p><!--There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, 
+                           or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't  It is a long established f
+                           act that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here,<br><br>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't  It is a long established f
+                           act that a reader will be distracted by the readable content of a page when looking at its layout.-->
+                           FOCUS TECHNOLOGY est une entreprise multisectoriel  qui œuvre dans le multimédia plus précisément dans la couverture médiatique des différentes cérémonies, c’est-à-dire le filmage et la photographie de ces dernières, mais aussi FOCUS TECHNOLOGY intervient égalementdans le domaine électrique car ayant les ingénieurs compétents à son sein pour rendre un service honnête et de qualité à sa clientèle.
+Pour arriver à ces fins, FOCUS TECHNOLOGY a des clients potentiels et d’autres qu’il espère gagner en offrant un excellent service mais également à s’assurant avoir des équipements de qualité et un personnel compétent afin de faire face à la concurrence car FOCUS TECHNOLOGY n’est pas la seule entreprise œuvrant dans ces domaines. 
+C’est ainsi que pour atteindre le maximum des personnes FOCUS TECHNOLOGY se base sur deux fronts : <br>
+-  <b>La compétence de son personnel<b> <br>
+-  <b>La qualité de ses matériels,<b>
+
+                        </p>
+                     </div>
+                     
+                  </div>
+               </div>
+
+</body>
+</html>
